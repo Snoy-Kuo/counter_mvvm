89a7badd316331e9d22e70aa2555c1dcc0bf3786
Snoy Kuo <snoy.kuo@twprotech.com>
2021-09-17 14:42:52 +0800

    copy counter remember code and paste.

---
+++ app/src/main/java/com/snoy/countermvvm/counter/CounterScreen.kt
@@ -0,0 +1,78 @@
+package com.snoy.countermvvm.counter
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.snoy.countermvvm.ui.theme.CounterMVVMTheme
+
+//ref 1 = https://developer.android.com/codelabs/jetpack-compose-state#5
+//ref 2 = https://developer.android.com/reference/kotlin/androidx/compose/runtime/saveable/package-summary#rememberSaveable(kotlin.Array,androidx.compose.runtime.saveable.Saver,kotlin.String,kotlin.Function0)
+
+@Composable
+fun CounterScreen() {
+    var counterValue by rememberSaveable { mutableStateOf(0) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                backgroundColor = MaterialTheme.colors.primary
+            ) {
+                Text(
+                    "Counter MVVM",
+                    modifier = Modifier
+                        .padding(10.dp),
+                    color = MaterialTheme.colors.onPrimary
+                )
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    counterValue++
+                }
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
+    ) {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "You have clicked the button this many times:",
+            )
+            CounterText(counterValue)
+        }
+    }
+}
+
+@Composable
+fun CounterText(counterValue: Int) {
+    Text(
+        text = "$counterValue",
+        style = MaterialTheme.typography.h4,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CounterMVVMTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            CounterScreen()
+        }
+    }
+}
--- app/src/main/java/com/snoy/countermvvm/ui/theme/Theme.kt
+++ app/src/main/java/com/snoy/countermvvm/ui/theme/Theme.kt
@@ -5,17 +5,21 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Teal200,
+    primaryVariant = Teal500,
+    secondary = Teal200,
+    secondaryVariant = Teal700
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Blue500,
+    primaryVariant = Blue700,
+    secondary = Blue200,
+    secondaryVariant = Blue700
 
     /* Other default colors to override
     background = Color.White,
@@ -36,6 +40,15 @@ fun CounterMVVMTheme(
         DarkColorPalette
     } else {
         LightColorPalette
+    }
+
+    // Remember a SystemUiController
+    val systemUiController = rememberSystemUiController()
+
+    SideEffect {
+        systemUiController.setStatusBarColor(
+            color = colors.primaryVariant
+        )
     }
 
     MaterialTheme(
